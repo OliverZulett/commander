614c43a88fb1996da62c672ce16511e0a02fcfb2
oliver zulett <oliverzulett@gmail.com>
2021-10-31 16:25:48 -0400

    controller created

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controller
+{
+  [ApiController]
+  // [Route("api/[controller]")] //cuando se le pone el [controller] agarra el nombre de la clase sin el Controller
+  [Route("api/commands")]
+  public class CommandsController : ControllerBase
+  {
+    private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+    public CommandsController()
+    {
+    }
+    
+    [HttpGet]
+    public ActionResult<IEnumerable<Command>> GetAllCommands()
+    {
+      var commandsItems = _repository.GetAppCommands();
+      return Ok(commandsItems);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Command> GetCommandById(int id)
+    {
+      var commandItem = _repository.GetCommandById(id);
+      return Ok(commandItem);
+    }
+  }
+}
