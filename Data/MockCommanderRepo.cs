77a99f78722c1d2e09dc00285b9d5e37599c3e24
oliver zulett <oliverzulett@gmail.com>
2021-10-31 16:09:27 -0400

    create mock repository and interface

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+  public class MockCommanderRepo : ICommanderRepo
+  {
+    public IEnumerable<Command> GetAppCommands()
+    {
+      var commands = new List<Command>
+      {
+        new Command{
+          Id=0,
+          HowTo="install node packages",
+          Line="npm i <package-name>",
+          Platform="all"
+        },
+        new Command{
+          Id=0,
+          HowTo="run node programa",
+          Line="npm run start",
+          Platform="all"
+        },
+        new Command{
+          Id=0,
+          HowTo="run node tests",
+          Line="npm run test",
+          Platform="all"
+        },
+      };
+      return commands;
+    }
+
+    public Command GetCommandById(int id)
+    {
+      return new Command
+      {
+        Id = 0,
+        HowTo = "installl node packages",
+        Line = "npm i <package-name>",
+        Platform = "all"
+      };
+    }
+  }
+}
