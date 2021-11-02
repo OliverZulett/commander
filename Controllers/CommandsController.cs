abc8f4b61cf16fed5beb3e0471cb3f9b5ca8d56f
oliver zulett <oliverzulett@gmail.com>
2021-11-01 22:11:22 -0400

    mapper implemented in controller

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using AutoMapper;
 using Commander.Data;
+using Commander.Dtos;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,26 +13,32 @@ namespace Commander.Controller
   public class CommandsController : ControllerBase
   {
     private readonly ICommanderRepo _repository;
+    private readonly IMapper _mapper;
 
     // private readonly MockCommanderRepo _repository = new MockCommanderRepo();
 
-    public CommandsController(ICommanderRepo repostory)
+    public CommandsController(ICommanderRepo repostory, IMapper mapper)
     {
       _repository = repostory;
+      _mapper = mapper;
     }
     
     [HttpGet]
-    public ActionResult<IEnumerable<Command>> GetAllCommands()
+    public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
     {
       var commandsItems = _repository.GetAllCommands();
-      return Ok(commandsItems);
+      return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandsItems));
     }
 
     [HttpGet("{id}")]
-    public ActionResult<Command> GetCommandById(int id)
+    public ActionResult<CommandReadDto> GetCommandById(int id)
     {
       var commandItem = _repository.GetCommandById(id);
-      return Ok(commandItem);
+      if (commandItem != null)
+      {
+        return Ok(_mapper.Map<CommandReadDto>(commandItem));
+      }
+      return NotFound();
     }
   }
 }
